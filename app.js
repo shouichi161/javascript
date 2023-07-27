e76b1dc3033ae9d81de3ccb137c28159ceda7a14
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-07-27 12:48:46 +0000

    [add]while文

--- app.js
+++ app.js
@@ -17,3 +17,14 @@ if(orange < apple){
 } else{
   alert('みかんの値段がりんごより高い');
 }
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
