ae174500bd8fb66f95c3d58f6b9241dfd30be176
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-07-28 10:21:14 +0000

    [add]関数を記述、呼び出し

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-let i;
-let num = 0;
+// let i;
+// let num = 0;
 
-for( i = 1; i < 11; i++){
-  num = num + i;
+// for( i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+// let number1 = 5;
+// let number2 = 5;
+
+// alert(number1 + '+' + number2 + 'は' + (number1 + number2) + 'です');
+// alert(number1 + '-' + number2 + 'は' + (number1 - number2) + 'です');
+// alert(number1 + '*' + number2 + 'は' + (number1 * number2) + 'です');
+// alert(number1 + '/' + number2 + 'は' + (number1 / number2) + 'です');
+
+// メイン部分
+let alertString;
+
+// 作成した関数を呼び出し、変数へ格納
+alertString = addString("webCamp");
+
+// 変数の中身をアラートで表示する
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
 }
-
-alert('1から10まで足し算した結果は' + num + 'です');
-
-let number1 = 5;
-let number2 = 5;
-
-alert(number1 + '+' + number2 + 'は' + (number1 + number2) + 'です');
-alert(number1 + '-' + number2 + 'は' + (number1 - number2) + 'です');
-alert(number1 + '*' + number2 + 'は' + (number1 * number2) + 'です');
-alert(number1 + '/' + number2 + 'は' + (number1 / number2) + 'です');
