230b3aa01c7f79d7727c292bebf59040902d9b15
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-08-24 13:09:57 +0000

    [add]javaファイルにjavascriptの記述

---
+++ java/js/javascript.js
@@ -0,0 +1,16 @@
+alert("Hello world");
+let hello = "Hello world";
+alert(hello);
+let int1 = 1;
+alert(int1);
+
+let orange = 100;
+let apple = 100;
+
+if (orange < apple){
+  alert('みかんの値段がりんごより安い')
+} else if(orange == apple){
+  alert('みかんとりんごは同じ値段')
+} else{
+  alert('みかんの値段がりんごより高い')
+}
